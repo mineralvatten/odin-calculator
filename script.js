86f1928d28e47b0e41f8b0c14b2c6a636a804a20
Mineralvatten <eetuvv@gmail.com>
2023-12-05 11:34:41 +0200

    improve basic layout

--- script.js
+++ script.js
@@ -30,12 +30,55 @@ buttonDiv = document.querySelector("#buttons");
 function createButtons() {
     for (i = 1; i <= 16; i++) {
         btn = document.createElement('div');
-        btn.textContent = `${i}`;
+        switch (i) {
+            case 4:
+                btn.textContent = "+";
+                break;
+            case 8:
+                btn.textContent = "-";
+                break;
+            case 12:
+                btn.textContent = "*";
+                break;
+            case 13:
+                btn.textContent = "0";
+                break;
+            case 14:
+                btn.textContent = ".";
+                break;
+            case 15:
+                btn.textContent = "CLR";
+                break;
+            case 16:
+                btn.textContent = "=";
+                break;
+            default:
+                if ((i < 4) && (i>0)) {
+                    btn.textContent = `${i}`;
+                }
+                else if ((i >=5) && (i <= 7)) {
+                    btn.textContent = `${i-1}`;
+                }
+                else {
+                    btn.textContent = `${i-2}`;
+                }
+        }
+        
         btn.style.fontSize= "16px";
-        btn.style.width = "85px";
-        btn.style.height = "102px";
+        btn.style.width = "81px";
+        btn.style.height = "98px";
+        btn.style.border = "1px";
+        btn.style.margin = "1px";
+        btn.style.borderStyle = "solid";
         btn.style.display = "flex";
+        btn.style.flexWrap = "wrap";
+        btn.style.fontSize = "32px";
+        btn.style.backround = "50,50,50";
+        btn.style.alignContent = "center";
+        btn.style.borderRadius = "4px";
+        btn.style.justifyContent = "center";
         btn.style.flex = "0 0 auto";
         buttonDiv.appendChild(btn);
     }
 }
+createButtons()
