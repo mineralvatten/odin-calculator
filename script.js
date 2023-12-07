db602ff1ff130393343295de8944f8e20e4a48b9
Mineralvatten <eetuvv@gmail.com>
2023-12-07 09:19:36 +0200

    + operator working

--- script.js
+++ script.js
@@ -26,16 +26,54 @@ let num1;
 let num2;
 let operator;
 const display = document.querySelector('#display');
+display.textContent = "";
+display.memory = "";
+display.writeFlag = false;
 function clearDisplay() {
     display.textContent = "";
 }
-function writeToDisplay(input) {
-    display.textContent += input;
+function writeToDisplay(event) {
+    if (display.textContent.length < 9) {
+        if (display.writeFlag == true) {
+            display.textContent = "";
+            display.writeFlag= false;
+        }
+        display.textContent += event.target.textContent;
+    }
 }
 function onOperatorClick(event) {
-    if (display.textContent != "") {
+    //if memory empty, display empty (unless using =), if something in memory
+/*     if (display.textContent != "") {
         display.memory = display.textContent + event.target.textContent;
         display.textContent = "";
+    } */
+    operatorKey = event.target.textContent;
+    switch (operatorKey) {
+        case "CLR":
+            display.memory = "";
+            display.textContent = "";
+            break;
+        case "=":
+            if (display.memory.length > 0) {
+                console.log(display.memory);
+            }
+            break;
+        case "+":
+            if (display.textContent.length > 0) {
+                if (display.memory.length == 0) {
+                    display.memory = Number(display.textContent);
+                    display.writeFlag = true;
+                }
+                else {
+                    display.memory += Number(display.textContent);
+                    display.writeFlag = true;
+                    display.textContent = display.memory;
+                }
+
+            }
+     
+        
+            
     }
 }
 buttonDiv = document.querySelector("#buttons");
@@ -44,24 +82,12 @@ function createButtons() {
         btn = document.createElement('div');
         switch (i) {
             case 4:
-                btn.textContent = "";
-                btnChild1 = document.createElement('div');
-                btnChild2 = document.createElement('div');
-                btn.appendChild(btnChild1);
-                btn.appendChild(btnChild2);
-                btnChild1.textContent = "+";
-                btnChild2.textContent = "-";
-                btnChild1.style.background = "green";
-                btnChild1.style.display = "flex";
-                btnChild1.style.flex = "1";
-                btnChild2.style.background = "pink";
-                btnChild2.style.flex = "1";
-                btnChild2.style.display = "flex";
-                //btn.addEventListener('click', (event) => onOperatorClick(event));
+                btn.textContent = "+";
+                btn.addEventListener('click', (event) => onOperatorClick(event));
                 break;
             case 8:
-                btn.textContent = "";
-                //btn.addEventListener('click', (event) => onOperatorClick(event));
+                btn.textContent = "-";
+                btn.addEventListener('click', (event) => onOperatorClick(event));
                 break;
             case 12:
                 btn.textContent = "*";
@@ -69,38 +95,38 @@ function createButtons() {
                 break;
             case 13:
                 btn.textContent = "0";
-                btn.addEventListener('click', (event) => display.textContent += event.target.textContent);
+                btn.addEventListener('click', (event) => writeToDisplay(event));
                 break;
             case 14:
                 btn.textContent = ".";
-                btn.addEventListener('click', (event) => display.textContent += event.target.textContent);
+                btn.addEventListener('click', (event) => writeToDisplay(event));
                 break;
             case 15:
                 btn.textContent = "CLR";
-                btn.addEventListener('click', clearDisplay);
+                btn.addEventListener('click', (event) => onOperatorClick(event));
                 break;
             case 16:
-                btn.textContent = "=";
-                btn.addEventListener('click', (event) => display.textContent += event.target.textContent);
+                btn.textContent = "/";
+                btn.addEventListener('click', (event) => onOperatorClick(event));
                 break;
             default:
                 if ((i < 4) && (i>0)) {
                     btn.textContent = `${i}`;
-                    btn.addEventListener('click', (event) => display.textContent += event.target.textContent);
+                    btn.addEventListener('click', (event) => writeToDisplay(event));
                 }
                 else if ((i >=5) && (i <= 7)) {
                     btn.textContent = `${i-1}`;
-                    btn.addEventListener('click', (event) => display.textContent += event.target.textContent);
+                    btn.addEventListener('click', (event) => writeToDisplay(event));
                 }
                 else {
                     btn.textContent = `${i-2}`;
-                    btn.addEventListener('click', (event) => display.textContent += event.target.textContent);
+                    btn.addEventListener('click', (event) => writeToDisplay(event));
                 }
         }
         
         btn.style.fontSize= "16px";
         btn.style.width = "81px";
-        btn.style.height = "98px";
+        btn.style.height = "80px";
         btn.style.border = "1px";
         btn.style.margin = "1px";
         btn.style.borderStyle = "solid";
@@ -114,6 +140,26 @@ function createButtons() {
         btn.style.flex = "0 0 auto";
         buttonDiv.appendChild(btn);
     }
+    btn = document.createElement('div');
+    buttonDiv.appendChild(btn);
+    btn.addEventListener('click', (event) => onOperatorClick(event));
+    btn.style.fontSize= "16px";
+    btn.style.width = "167px";
+    btn.style.height = "60px";
+    btn.style.border = "1px";
+    btn.style.margin = "1px";
+    btn.style.borderStyle = "solid";
+    btn.style.display = "flex";
+    btn.style.flexWrap = "wrap";
+    btn.style.fontSize = "32px";
+    btn.style.background = "rgb(50,50,50)";
+    btn.style.alignContent = "center";
+    btn.style.borderRadius = "4px";
+    btn.style.justifyContent = "center";
+    btn.style.marginLeft = "85px"
+    btn.textContent = "=";
+    //btn.style.flex = "1 0 auto";
+    //btn.style.justify = "stretch";
 }
 createButtons()
 
